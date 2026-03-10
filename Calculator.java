135d9bb8194530c6ed8529431bd6b0750f53135c
withkenik <44743442+withkenik@users.noreply.github.com>
2026-03-11 08:48:18 +0900

    이창익_과제제출 2
    웹훅 컨트롤러 테스트 중~

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ public class Calculator{
 
     // 1. 새로 구현한 add() 메서드 
     public int add(int a, int b){        
-        return 0; 
+        int result = a + b ; // 26. 웹훅 컨트롤러 테스트 
+        return result; 
     }
 }
