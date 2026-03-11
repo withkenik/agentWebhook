5ba98473b5459cb95893351dddfd0a1b34b09f8c
withkenik <44743442+withkenik@users.noreply.github.com>
2026-03-11 09:03:15 +0900

    이창익_과제제출 3

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,9 @@
 public class Calculator{
 
-    // 1. 새로 구현한 add() 메서드 
+    // 2. 새로 구현한 add() 메서드 
     public int add(int a, int b){        
-        int result = a + b ; // 26. 웹훅 컨트롤러 테스트 
-        return result; 
+//        int result = a + b ; // 26. 웹훅 컨트롤러 테스트 
+//        return result; 
+        return a + b;
     }
 }
